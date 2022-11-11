734aaf9c09bb5fbe270d383d7af75ab84f3ca719
Ian M <44310933+mnai01@users.noreply.github.com>
2022-11-11 03:26:33 -0500

    Update group item route to accept body

--- src/handlers/put-grouped-items.js
+++ src/handlers/put-grouped-items.js
@@ -2,7 +2,6 @@
 
 // Create a DocumentClient that represents the query to add an item
 const AWS = require("aws-sdk");
-const data = require("../../sample.json");
 const { checkRequirements } = require("../helper/checkRequirements");
 // Get the DynamoDB table name from environment variables
 const tableName = process.env.HOUSE_PLANTS_TABLE;
@@ -40,12 +39,15 @@ exports.putGroupedItemsHandler = async (event) => {
   // Create a DocumentClient that represents the query to add an item
   const docClient = new AWS.DynamoDB.DocumentClient(settings);
 
+  // Get id and name from the body of the request
+  const body = JSON.parse(event.body);
+
   try {
     let temp = [];
-    await data.HousePlantsTable.forEach((record) => {
+    await body.HousePlantsTable.forEach((record) => {
       temp.push(record);
       // TODO If at last index push the remaining in the array
-      if (temp.length === 2) {
+      if (temp.length === 25) {
         const params = { RequestItems: { [tableName]: temp } };
         docClient.batchWrite(params, function (err, data) {
           if (err) console.info({ err, data }); // an error occurred
